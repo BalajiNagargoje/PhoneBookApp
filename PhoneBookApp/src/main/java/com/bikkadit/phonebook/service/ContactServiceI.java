a8b69abffbba2925e8ee138ee1a6020ce29e16fc
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-11-11 01:25:10 +0530

    PHON-5 ;Implementation of service layer

--- PhoneBookApp/src/main/java/com/bikkadit/phonebook/service/ContactServiceI.java
+++ PhoneBookApp/src/main/java/com/bikkadit/phonebook/service/ContactServiceI.java
@@ -1,8 +1,19 @@
 package com.bikkadit.phonebook.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.bikkadit.phonebook.entity.Contact;
 
 @Service
 public interface ContactServiceI {
+	
+	boolean saveContact(Contact contact);
+	List<Contact> getAllContacts();
+	
+	Contact getContactById(Integer cid);
+	
+	boolean deleteContactById(Integer cid);
 
 }
